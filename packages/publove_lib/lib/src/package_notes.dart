0ed0c4f66e8bb7e7e29deb448fed4dbe48aadf51
Remi Rousselet <darky12s@gmail.com>
2024-10-01 17:21:35 +0200

    Update package_notes.dart

--- packages/publove_lib/lib/src/package_notes.dart
+++ packages/publove_lib/lib/src/package_notes.dart
@@ -1,5 +1,5 @@
 final packageNotes = <String, String>{
   // csells, 2024-09-25
-  'provider': 'the author of provider recommends using riverpod instead',
+  'provider': 'Provider is considered feature-complete and will not receive significant updates, unless an important change to Dart/Flutter happens in the future.',
   // add more notes here as appropriate
 };
